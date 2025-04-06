bac165411ff3803c84f7b0061d7036224f38e4fa
igorp120 <ipasech@gmail.com>
2025-04-06 17:58:59 -0400

    scaffolding

--- src/react/RootComponent.tsx
+++ src/react/RootComponent.tsx
@@ -1,24 +1,15 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { post } from './helpers/api';
-// import { AgGridReact } from 'ag-grid-react';
-// import 'ag-grid-community/styles/ag-grid.css';
-// import 'ag-grid-community/styles/ag-theme-alpine.css';
-// import { ColDef } from 'ag-grid-community';
-// import { ServerSideRowModelModule, IServerSideDatasource, IServerSideGetRowsParams } from 'ag-grid-community';
-
-const testData = [
-  { name: 'John Doe', age: 10, city: 'Toronto'},
-  { name: 'James Cameron', age: 11, city: 'New York'},
-  { name: 'Fannie Mae', age: 20, city: 'San Francisco'},
-];
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { ColDef } from 'ag-grid-community';
 
 interface Props {
-  // id: string;
-  // onDataRequest?: (params: IServerSideGetRowsParams) => void;
 }
 
 export const RootComponent: React.FC<Props> = () => {
-  const [data, setData] = useState<any[]>(testData);
+  const [data, setData] = useState<any[]>([]);
 
   useEffect(() => {
       console.log('cookies', document.cookie);
@@ -32,9 +23,41 @@ export const RootComponent: React.FC<Props> = () => {
       fetchData();
   }, []);
 
+  const [gridApi, setGridApi] = useState<any>(null);
+
+  const columnDefs: ColDef[] = useMemo(() => [
+    { headerName: 'Name', field: 'name', sortable: true, filter: 'agTextColumnFilter' },
+    { headerName: 'Age', field: 'age', sortable: true, filter: 'agNumberColumnFilter' },
+    { headerName: 'City', field: 'city', sortable: true, filter: 'agTextColumnFilter' },
+  ], []);
+
+  const defaultColDef = useMemo(() => ({
+    flex: 1,
+    minWidth: 100,
+    resizable: true,
+    filter: true,
+  }), []);
+
+
+  const onGridReady = useCallback((params: any) => {
+    const gridApi = params.api;
+    setGridApi(gridApi);
+    console.log('ag-grid ready', gridApi);
+    // params.api.setServerSideDatasource(dataSource);
+  }, []);
+
+  // themes:  ag-theme-balham-dark, ag-theme-quartz-dark, ag-theme-alpine-dark
   return (
-    <div>
-      <h1>My React App</h1>
+    <div className="ag-theme-alpine-dark" style={{ height: 'calc(100vh - 17rem)', width: '100%' }}>
+      <AgGridReact
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}
+        onGridReady={onGridReady}
+        rowData={data}
+        className='ag-theme-alpine-dark'
+      />
     </div>
+
   );
 }
+
